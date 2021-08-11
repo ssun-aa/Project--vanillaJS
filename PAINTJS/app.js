a8f87ef48948ab973488b4166212fc075def7d91
ssunah <ssk6200@daum.net>
2021-08-11 23:12:09 +0900

    Feat : change mode [paint/fill]

--- PAINTJS/app.js
+++ PAINTJS/app.js
@@ -1,14 +1,25 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const save = document.getElementById("jsSave");
 
-canvas.width = 700;
-canvas.height = 700;
+const DEFAULT_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";//canvas 초기 배경 설정
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+
+ctx.strokeStyle = DEFAULT_COLOR;
+ctx.fillStyle = DEFAULT_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting(){
     painting = false;
@@ -33,6 +44,32 @@ function onMouseMove(event){
 function changeColor(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+function changeLineWidth(event){
+    const width = event.target.value;
+    ctx.lineWidth = width;
+}
+
+function changeMode(event){
+    if(filling){
+        event.target.innerHTML = "FILL";
+        filling = false;
+    } else{
+        event.target.innerHTML = "PAINT"
+        filling = true;
+        
+    }
+}
+
+function canvasClick(){
+    if(filling)
+        ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+}
+
+function handleCM(event){//우클릭 방지
+    event.preventDefault();
 }
 
 if(canvas){
@@ -40,9 +77,19 @@ if(canvas){
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", canvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 Array.from(colors).forEach(color => 
     color.addEventListener("click",  changeColor)
 );
+
+if(range){
+    range.addEventListener("input", changeLineWidth);
+}
+
+if(mode){
+    mode.addEventListener("click", changeMode);
+}
 
